2a5b8e5f54b1c61ed84df55f707e1d9a28f436f3
mturnerdesigns <meaghan.turner9@gmail.com>
2017-04-14 22:27:01 -0400

    play with datefmt

--- app/Event.php
+++ app/Event.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    protected $fillable = ['event_title', 'event_description', 'event_location', 'event_date', 'event_time'];
+    protected $fillable = [
+      'event_title',
+      'event_description',
+      'event_location',
+      'event_date',
+      'event_time',
+      ];
 
 }
